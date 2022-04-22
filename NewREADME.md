6f5e595e20861782736fba9401e3fb40a07d7d33
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-04-21 23:03:03 -0700

    test read me generator

--- NewREADME.md
+++ NewREADME.md
@@ -1,11 +1,10 @@
 
-  # read me generator 
+  #  
 
   ## Description
   
-    here is a read me generator
+    
   
-  GPL 3.0
 
   ## Table of Contents
   
@@ -18,27 +17,27 @@
   
   ## Installation
   
-  this is how you install
+  
   
   
   ## Usage 
   
-  this is how you use
+  
   
   
   ## Credits
   
-  sophia custodia
-  * sophia.custodia@gmail.com
-  * github.com/sophtron5000
-  * linkedin.com/in/sophiacustodia
+  
+  * [Email]()
+  * [GitHub]()
+  * [LinkedIn]()
     
   ## Contributing
   
   If you would like to contribute to this code please email me for instructions on how: 
-  [sophia custodia](sophia.custodia@gmail.com)
+  []()
   
   ## Tests
   
   If you would like instruction on how to test, please email me: 
-  [sophia custodia](sophia.custodia@gmail.com)
+  []()
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,50 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(data) {
+//   const expression = `${data.license}`;
+// switch (expression) {
+//   case 'MIT':
+//     expression = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+//     break;
+//   case 'GPL 3.0':
+//     expression = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+//     break;
+//   default:
+//     expression = "";
+// }
+// }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if ()
-}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink() {
+//   const licenseLink = renderLicenseBadge(license);
+//   switch (licenseLink) {
+//     case 'MIT':
+//       licenseLink = `[License: MIT](https://opensource.org/licenses/MIT)`;
+//       break;
+//     case 'GPL 3.0':
+//       licenseLink = `[License: GPL v3](https://www.gnu.org/licenses/gpl-3.0)`;
+//       break;
+//     default:
+//       licenseLink = "";
+//   }
+// }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection() {
+//   const licenseSection = renderLicenseLink(license);
+//   switch (licenseSection) {
+//     case 'MIT':
+//       licenseSection = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+//       break;
+//     case 'GPL 3.0':
+//       licenseSection = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+//       break;
+//     default:
+//       licenseSection = "";
+//   }
+// }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -21,7 +55,6 @@ function generateMarkdown(data) {
   
     ${data.description}
   
-  ${data.license}
 
   ## Table of Contents
   
